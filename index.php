43bb65296ecd556fbf4acfd55a4bf554f474af8f
abhilash2200 <ab.digitalwolf@gmail.com>
2026-02-27 11:08:04 +0530

    Add testimonials and walkthrough pages with responsive design and animations

--- index.php
+++ index.php
@@ -15,145 +15,7 @@
 </head>
 
 <body id="container">
-    <div class="header">
-        <div class="d-flex justify-content-between align-items-center">
-            <a href="index.html">
-                <img src="./assets/img/logo/logo.png" alt="logo" width="100%" height="80px">
-            </a>
-
-            <img class="burger-menu" src="./assets/icons/hamburgers.png" alt="" type="button" data-bs-toggle="offcanvas"
-                data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
-        </div>
-        <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
-            <div class="offcanvas-header">
-                <img src="./assets/img/logo/logo.png" alt="logo" width="4%" height="80px">
-                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                    aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <div class="row">
-                    <div class="col-lg-8 d-lg-block d-none" style="height: 100vh; background-color: black;">
-                        <div class="row">
-                            <div class="col-lg-6"></div>
-                            <div class="col-lg-6 p-3">
-                                <div class="p-3 d-flex justify-content-between align-items-center">
-                                    <h5 class="text-light mb-0">READ OUR BLOG</h5>
-                                    <a href="#">
-                                        <button class="btn-class-name">
-                                            <span>Explore More</span>
-                                        </button>
-                                    </a>
-                                </div>
-                                <ul class="ps-1" style="height: 70vh; padding-bottom: 20px; overflow-y: scroll;">
-                                    <li>
-                                        <div class="card mb-3" style="max-width: 540px;">
-                                            <div class="row g-0">
-                                                <div class="col-md-4">
-                                                    <img src="./assets/img/projects/textMen.png"
-                                                        class="img-fluid rounded-start" alt="...">
-                                                </div>
-                                                <div class="col-md-8">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card titleCard titleCard titleCard title
-                                                        </h5>
-                                                        <p class="card-text">This is a wider card with supporting text
-                                                            below as a natural lead-in to additional content.</p>
-                                                        <p class="card-text"><small class="text-muted">Last updated : 26
-                                                                June 2023</small></p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="card mb-3" style="max-width: 540px;">
-                                            <div class="row g-0">
-                                                <div class="col-md-4">
-                                                    <img src="./assets/img/projects/textMen.png"
-                                                        class="img-fluid rounded-start" alt="...">
-                                                </div>
-                                                <div class="col-md-8">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">This is a wider card with supporting text
-                                                            below as a natural lead-in to additional content. This
-                                                            content is a little bit longer.</p>
-                                                        <p class="card-text"><small class="text-muted">Last updated : 26
-                                                                Mar 2023</small></p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="card mb-3" style="max-width: 540px;">
-                                            <div class="row g-0">
-                                                <div class="col-md-4">
-                                                    <img src="./assets/img/projects/textMen.png"
-                                                        class="img-fluid rounded-start" alt="...">
-                                                </div>
-                                                <div class="col-md-8">
-                                                    <div class="card-body">
-                                                        <h5 class="card-title">Card title</h5>
-                                                        <p class="card-text">This is a wider card with supporting text
-                                                            below as a natural lead-in to additional content. This
-                                                            content is a little bit longer.</p>
-                                                        <p class="card-text"><small class="text-muted">Last updated : 07
-                                                                April 2023</small></p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="Text svelte-mtggno">
-                            <a href="animation-course.html">
-                                <h3 role="presentation" class="heading1 svelte-mtggno" style="cursor: pointer;">
-                                    Animation Course</h3>
-                            </a>
-                            <a href="vfx-course.html">
-                                <h3 role="presentation" class="heading1 svelte-mtggno" style="cursor: pointer;">Vfx
-                                    Course</h3>
-                            </a>
-                            <a href="graphics-design-course.html">
-                                <h3 role="presentation" class="heading1 svelte-mtggno" style="cursor: pointer;">Graphics
-                                    Design Course</h3>
-                            </a>
-                            <a href="web-design-course.html">
-                                <h3 role="presentation" class="heading1 svelte-mtggno" style="cursor: pointer;">Web
-                                    Design Course</h3>
-                            </a>
-                            <hr class="hr-css svelte-mtggno">
-                            <div class="subnavText">
-                                <a href="about-us.html">
-                                    <p>About Us</p>
-                                </a>
-                                <a href="our-team.html">
-                                    <p>Our Team</p>
-                                </a>
-                                <a href="testimonials.html">
-                                    <p>Testimonials</p>
-                                </a>
-                                <a href="blog.html">
-                                    <p>Blog</p>
-                                </a>
-                                <a href="career.html">
-                                    <p>Career</p>
-                                </a>
-                                <a href="contact-us.html">
-                                    <p>Contact Us</p>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include 'components/header.php'; ?>
     <div class="main" id="MAIN">
         <section class="sec_one">
             <div class="row hero-sec g-0">
@@ -255,7 +117,7 @@
                                         Play in an ever changing world in which you aren’t merely a player and also a
                                         creator and administrator.
                                     </p>
-                                    <a class="btn2" href="animation-course.html">Learn More</a>
+                                    <a class="btn2" href="animation.html">Learn More</a>
                                 </div>
                             </div>
                             <div class="info-img">
@@ -275,7 +137,7 @@
                                         Play in an ever changing world in which you aren’t merely a player and also a
                                         creator and administrator.
                                     </p>
-                                    <a class="btn3" href="vfx-course.html">Learn More</a>
+                                    <a class="btn3" href="walkthrough.html">Learn More</a>
                                 </div>
                             </div>
                             <div class="item2-img">
@@ -295,7 +157,7 @@
                                         Play in an ever changing world in which you aren’t merely a player and also a
                                         creator and administrator.
                                     </p>
-                                    <a class="btn3" href="graphics-design-course.html">Learn More</a>
+                                    <a class="btn3" href="educational-tech.html">Learn More</a>
                                 </div>
                             </div>
                             <div class="item3-img">
@@ -315,7 +177,7 @@
                                         Play in an ever changing world in which you aren’t merely a player and also a
                                         creator and administrator.
                                     </p>
-                                    <a class="btn3" href="graphics-design-course.html">Learn More</a>
+                                    <a class="btn3" href="explainer-video.html">Learn More</a>
                                 </div>
                             </div>
                             <div class="item4-img">
@@ -335,7 +197,7 @@
                                         Play in an ever changing world in which you aren't merely a player and also a
                                         creator and administrator.
                                     </p>
-                                    <a class="btn3" href="graphics-design-course.html">Learn More</a>
+                                    <a class="btn3" href="ai-content.html">Learn More</a>
                                 </div>
                             </div>
                             <div class="item5-img">
@@ -576,48 +438,8 @@
         </div>
     </div>
     <!-- Modal -->
-    <div class="modal fade" id="popup1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
-        style="background: #00000070;backdrop-filter: blur(1px);">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content bg-dark text-white">
-                <div class="modal-header border-0 p-0 position-relative">
-                    <svg class="hero-svg-text my-0" viewBox="0 0 1320 120" style="width: 100%;">
-                        <text x="50%" y="50%" dy=".35em" text-anchor="middle">
-                            Admission Open
-                        </text>
-                    </svg>
-                    <button type="button" class="btn-close bg-light position-absolute top-0 end-0 m-2"
-                        data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body p-0">
-                    <div class="login-box">
-                        <form>
-                            <div class="user-box">
-                                <input type="text" name="name" required="">
-                                <label>Name</label>
-                            </div>
-                            <div class="user-box">
-                                <input type="email" name="email" required="">
-                                <label>Email</label>
-                            </div>
-                            <div class="user-box">
-                                <input type="tel" name="phone" required="">
-                                <label>Phone No.</label>
-                            </div>
-                            <div class="user-box">
-                                <input type="text" name="course" required="">
-                                <label>Course</label>
-                            </div>
-                            <a href="#">
-                                Apply Now
-                                <span></span>
-                            </a>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include 'components/popup.php'; ?>
+    
     <script src="./assets/js/jquery.min.js"></script>
     <script src="./assets/js/bootstrap.bundle.min.js"></script>
     <!-- GSAP Core & Plugins -->
