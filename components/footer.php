93e6f7bba5ea2d0fdb880e634cdc647ce4f63c2b
abhilash2200 <ab.digitalwolf@gmail.com>
2026-02-27 11:34:32 +0530

    Refactor footer implementation by creating footer.php component to reduce code duplication across multiple pages

---
+++ components/footer.php
@@ -0,0 +1,55 @@
+<div class="last-section">
+    <div class="keep">
+        <p class="fw-bold fs-4 mb-1">Who we are</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, totam. Exercitationem suscipit,
+            doloribus sequi tempora, </p>
+        <p class="fw-bold">KEEP UP TO DATE</p>
+    </div>
+    <div class="login-box">
+        <form>
+            <div class="user-box">
+                <input type="text" name="name" required="">
+                <label>Name</label>
+            </div>
+            <div class="user-box">
+                <input type="email" name="email" required="">
+                <label>Email</label>
+            </div>
+            <div class="user-box">
+                <input type="tel" name="phone" required="">
+                <label>Phone No.</label>
+            </div>
+            <div class="user-box">
+                <input type="text" name="course" required="">
+                <label>Course</label>
+            </div>
+            <a href="#">
+                Apply Now
+                <span></span>
+            </a>
+        </form>
+    </div>
+    <div class="social-div-main">
+        <hr class="w-50 text-white">
+        <div class="social-div">
+            <i class="fab fa-twitter"></i>
+            <a class="text-white" href="#" rel="noreferrer">
+                Twitter
+            </a>
+        </div>
+        <hr class="w-50 text-white">
+        <div class="social-div">
+            <i class="fab fa-youtube"></i>
+            <a class="text-white" href="#" rel="noreferrer">
+                Youtube
+            </a>
+        </div>
+        <hr class="w-50 text-white">
+        <div class="social-div">
+            <i class="fab fa-facebook-f"></i>
+            <a class="text-white" href="#" rel="noreferrer">
+                Facebook
+            </a>
+        </div>
+    </div>
+</div>
